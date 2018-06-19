eea124493e158c13530610a39134b6065971defa
Nikolay Martynov <tty0sh@gmail.com>
2018-06-19 23:12:34 +0300

    fixed path to Layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Route } from 'react-router-dom';
 
 import Header from 'components/Header';
@@ -6,9 +6,9 @@ import Dashboard from 'components/Dashboard';
 import Projects from 'components/Projects';
 
 export default () => (
-  <div>
+  <Fragment>
     <Header />
     <Route path="/" component={Dashboard} exact />
     <Route path="/projects" component={Projects} exact />
-  </div>
+  </Fragment>
 );
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 
 import Login from 'components/Login';
-import Layout from 'components/Main';
+import Layout from 'components/Layout';
 
 const appRouter = () => (
   <BrowserRouter>
