d7a5d52ef02ed64aac6034eac536b2834cc04880
Nikolay Martynov <tty0sh@gmail.com>
2018-07-01 17:31:25 +0300

    refactor Login component with recompose

---
+++ src/components/Login/LoginForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Form, Field } from 'react-final-form';
+import { renderInputWithError } from 'helpers/validation';
+
+import './login.scss';
+
+const loginForm = ({ isPending, onSubmit }) => (
+  <div className="login__container">
+    <div className="login__title">Tasky</div>
+    <div className="login__bg">
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit, pristine, invalid }) => (
+          <form name="loginForm" onSubmit={handleSubmit} className="login__form">
+            <Field name="login" type="text" placeholder="Login" component={renderInputWithError} />
+            <Field name="password" type="password" placeholder="Password" component={renderInputWithError} />
+            <button type="submit" className="login__button" disabled={isPending}>
+              Sign In
+            </button>
+          </form>
+        )}
+      />
+    </div>
+  </div>
+);
+
+export default loginForm;
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,43 +1,15 @@
-import React from 'react';
-import { Form, Field } from 'react-final-form';
+import { compose, withHandlers } from 'recompose';
 import { connect } from 'react-redux';
 import { login } from 'actions/auth';
-import { renderInputWithError } from 'helpers/validation';
+import loginForm from './LoginForm';
 
-import './login.scss';
+const onSubmit = props => data => {
+  const { login, history } = props;
 
-class loginForm extends React.Component {
-  onSubmit = data => {
-    const { login, history } = this.props;
-
-    login(data).then(() => {
-      history.push('/');
-    });
-  };
-
-  render() {
-    const { isPending } = this.props;
-    return (
-      <div className="login__container">
-        <div className="login__title">Tasky</div>
-        <div className="login__bg">
-          <Form
-            onSubmit={this.onSubmit}
-            render={({ handleSubmit, pristine, invalid }) => (
-              <form name="loginForm" onSubmit={handleSubmit} className="login__form">
-                <Field name="login" type="text" placeholder="Login" component={renderInputWithError} />
-                <Field name="password" type="password" placeholder="Password" component={renderInputWithError} />
-                <button type="submit" className="login__button" disabled={isPending}>
-                  Sign In
-                </button>
-              </form>
-            )}
-          />
-        </div>
-      </div>
-    );
-  }
-}
+  login(data).then(() => {
+    history.push('/');
+  });
+};
 
 const mapStateToProps = ({ auth }) => ({
   isPending: auth.isPending,
@@ -47,7 +19,12 @@ const mapDispatchToProps = dispatch => ({
   login: data => dispatch(login(data)),
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+export default compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  withHandlers({
+    onSubmit,
+  })
 )(loginForm);
