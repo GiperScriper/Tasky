55edcf8807cd0d8a26e942af569cc791f9c6476b
dfkadyr <dfkadyr@gmail.com>
2018-07-04 23:34:16 +0300

    Implement Login Page UI layout

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -3,9 +3,9 @@ import React from 'react';
 export const required = value => (!value ? 'this field is required.' : '');
 
 export const renderInputWithError = ({ input, meta, type, label, placeholder }) => (
-  <div className="login__form-item">
+  <div className={`login__item login__item--${type}`}>
     {label && <label>{label}</label>}
-    <input type={type} {...input} placeholder={placeholder} className="login__form-input" spellCheck="false" />
+    <input type={type} {...input} placeholder={placeholder} className="login__input" spellCheck="false" />
     {meta.touched && meta.error && <div className="login__form-error">{meta.error}</div>}
   </div>
 );
