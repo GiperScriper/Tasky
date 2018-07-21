4b6c55ab18fd23e4f703eb34a6f618dcc92e2496
dfkadyr <dfkadyr@gmail.com>
2018-07-21 18:55:25 +0300

    Edit style for buttons

--- src/components/Projects/ProjectAddItem/ProjectForm.js
+++ src/components/Projects/ProjectAddItem/ProjectForm.js
@@ -12,10 +12,10 @@ export default class ProjectForm extends Component {
         <input type="text" placeholder="Project name" className="additing-form__input" />
         <textarea placeholder="Project description" className="additing-form__textarea" />
         <div className="projects__footer">
-          <button className="button button--cancel" type="button" onClick={this.props.toggleItemView}>
+          <button className="button button--secondary" type="button" onClick={this.props.toggleItemView}>
             Cancel
           </button>
-          <button className="button button--success" type="submit">
+          <button className="button button--primary" type="submit">
             Save
           </button>
         </div>
