3795d9a6bc327a52f36f241312e1e281bb93925c
Nikolay Martynov <tty0sh@gmail.com>
2018-08-07 20:48:14 +0300

    updated project tests

--- src/tests/fixtures/projects.js
+++ src/tests/fixtures/projects.js
@@ -1,23 +1,33 @@
+import moment from 'moment';
+
 const projects = [
   {
     id: 1,
+    creatorId: 'xdsdfgfdgfgf',
     title: 'Sample 1',
     description: 'some sample project 1',
+    createdAt: moment(1533663785792),
   },
   {
     id: 2,
+    creatorId: 'xdsdfgfdgfgf2',
     title: 'Sample 2',
     description: 'some sample project 2',
+    createdAt: moment(1533663785792),
   },
   {
     id: 3,
+    creatorId: 'xdsdfgfdgfgf3',
     title: 'Sample 3',
     description: 'some sample project 3',
+    createdAt: moment(1533663785792),
   },
   {
     id: 4,
+    creatorId: 'xdsdfgfdgfgf4',
     title: 'Sample 4',
     description: 'some sample project 4',
+    createdAt: moment(1533663785792),
   },
 ];
 
